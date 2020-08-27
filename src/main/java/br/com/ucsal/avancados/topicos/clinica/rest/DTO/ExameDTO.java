ae3057530b5866095edba736f312f8b29f54e620
Jean Lima <jean.jr.souza@hotmail.com>
2020-08-27 15:35:49 -0300

    Controller funcionario - ok

---
+++ src/main/java/br/com/ucsal/avancados/topicos/clinica/rest/DTO/ExameDTO.java
@@ -0,0 +1,14 @@
+package br.com.ucsal.avancados.topicos.clinica.rest.DTO;
+
+
+
+public class ExameDTO {
+
+    private Integer id;
+    private Integer medico;
+
+    private Integer funcionario;
+
+    private String tipoExame;
+
+}
